ac3e1637528f18640bf3478908f8f8beed5be5d1
greedman231 <93518502+greedman231@users.noreply.github.com>
2021-12-14 12:40:22 +0300

    T-0
    ParaByValueDemo

---
+++ ParaByValueDemo/src/com/company/ParaByValue.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class ParaByValue {
+    void callByVal(int x, int y) {
+        x=x+y;
+        y = y + 1;
+        System.out.println("x =" + x);
+        System.out.println("y =" + y);
+    }
+}
